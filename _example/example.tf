57396919557338c58b211f7ec2bfbc82f732fbc3
theprashantyadav <theprashantyadav7@gmail.com>
2022-12-02 18:40:49 +0530

    Added member resource

--- _example/example.tf
+++ _example/example.tf
@@ -24,11 +24,9 @@ module "bucket" {
 
   location                                  = "US"
   project_id                                = "clouddrove"
-  google_storage_bucket_iam_policy_enabled  = true
-  google_storage_bucket_iam_binding_enabled = true
   google_storage_bucket_iam_member_enabled  = true
   bucket_id                                 = module.bucket.bucket.id
-  member                                    = ""
+//  member                                    = ""
 
 
 
